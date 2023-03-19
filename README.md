eb63710aa277a6ee06aa45a642c07580f8325257
la <alvispets@gmail.com>
2023-03-20 00:29:22 +0800

    add new

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vueDemo_Eleme_Ts
 饿了么demo-TS版本
+
+npm install  create-vite@latest XXXXX
+
---
+++ vue3-ts-vite-eleapp/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import Index from "./views/index.vue";
+
+</script>
+
+<template>
+  <div>
+   <Index/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
